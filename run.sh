d6f0fc99d1887e9ddc6e27f8e6f9ba46a67136f1
Matthew Barton <wf5617@M0988-Mac.local>
2022-11-19 13:38:30 +0000

    Update of documentation test

--- run.sh
+++ run.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+git rev-parse remotes/origin/main >> hash.txt
+diff hash.txt ./main/hash.txt > /dev/null 2>&1
+error=$? 
+if [ $error -eq 1 ]
+then
+rm -rf ./main/
 git checkout main -- main
 cd ./main
 bash buildwebsite.sh
 cd ..
+rm hash.txt
+fi
+
+git rev-parse remotes/origin/Experimental >> hash.txt
+diff hash.txt ./Experimental/hash.txt > /dev/null 2>&1
+error=$?
+if [ $error -eq 1 ]
+then
+rm -rf ./Experimental/
 git checkout Experimental --  Experimental
-cd Experimental
+cd ./Experimental
 bash buildwebsite.sh
 cd ..
+rm hash.txt
+fi
+
 cp ./main/sans-serif-lwarp-sagebrush.css ./
 gfortran-mp-12 homepage.f90
 ./a.out >> index.html
