b38ec4cdc53d485ab711ec93bae9b6e7a2930bac
Matthew <Matthew@Matthews-MacBook-Pro-2.local>
2022-11-19 18:54:17 +0000

    Change

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git rev-parse main >> hash.txt
+git rev-parse remotes/origin/main >> hash.txt
 diff hash.txt ./main/hash.txt > /dev/null 2>&1
 error=$? 
 if [ $error -eq 1 ]
@@ -8,21 +8,21 @@ rm -rf ./main/
 git checkout remotes/origin/main -- main
 cd ./main
 bash buildwebsite.sh
+mv ../hash.txt .
 cd ..
-rm hash.txt
 fi
 
-git rev-parse Experimental >> hash.txt
+git rev-parse remotes/origin/Experimental >> hash.txt
 diff hash.txt ./Experimental/hash.txt > /dev/null 2>&1
 error=$?
 if [ $error -eq 1 ]
 then
 rm -rf ./Experimental/
-git checkout  remotes/origin/Experimental --  Experimental
+git checkout remotes/origin/Experimental --  Experimental
 cd ./Experimental
 bash buildwebsite.sh
+mv ../hash.txt .
 cd ..
-rm hash.txt
 fi
 
 cp ./main/sans-serif-lwarp-sagebrush.css ./
