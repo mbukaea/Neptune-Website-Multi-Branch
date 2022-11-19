92a2822cffef2693b16b7f7a1ae209232f218a42
Matthew <Matthew@Matthews-MacBook-Pro-2.local>
2022-11-19 18:19:38 +0000

    Change

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 git rev-parse main >> hash.txt
 diff hash.txt ./main/hash.txt > /dev/null 2>&1
 error=$? 
-if [ $error -neq 0 ]
+if [ $error -eq 1 ]
 then
 rm -rf ./main/
 git checkout remotes/origin/main -- main
@@ -15,7 +15,7 @@ fi
 git rev-parse Experimental >> hash.txt
 diff hash.txt ./Experimental/hash.txt > /dev/null 2>&1
 error=$?
-if [ $error -neq 0 ]
+if [ $error -eq 1 ]
 then
 rm -rf ./Experimental/
 git checkout  remotes/origin/Experimental --  Experimental
