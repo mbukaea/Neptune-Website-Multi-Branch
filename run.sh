d1fc9108cd9023ced5dc6b41cae41393512a18a1
Matthew Barton <matthewbarton@Matthews-MacBook-Pro-2015.local>
2022-11-20 10:21:00 +0000

    Change

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@ diff hash1.txt ./main/hash.txt > /dev/null 2>&1
 error=$? 
 if [ $error -eq 1 ]
 then
+num1=$(git log -1 --pretty="format:%ct" ./main)
+num2=$(git log Deployment..main -1 --pretty="format:%ct" ./main)
+if ["$num2" -gt "$num1"]
+then 
 rm -rf ./main/
 git checkout remotes/origin/main -- main
 cd ./main
@@ -13,10 +17,15 @@ rm hash.txt
 git rev-parse remotes/origin/main >> hash.txt
 cd ..
 fi
+fi
 git rev-parse remotes/origin/Experimental >> hash2.txt
 diff hash2.txt ./Experimental/hash.txt > /dev/null 2>&1
 error=$?
 if [ $error -eq 1 ]
+then
+num1=$(git log -1 --pretty="format:%ct" ./Experimental)
+num2=$(git log Deployment..Experimental -1 --pretty="format:%ct" ./Experimental)
+if ["$num2" -gt "$num1"]
 then
 rm -rf ./Experimental/
 git checkout remotes/origin/Experimental --  Experimental
@@ -25,6 +34,7 @@ bash buildwebsite.sh
 rm hash.txt
 git rev-parse remotes/origin/Experimental >> hash.txt
 cd ..
+fi
 fi
 gfortran homepage.f90
 ./a.out >> index.html
